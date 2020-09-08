d38644171f9bb23bc8bce61ce1fdcf0ebb1e486f
Ryan <yanvalue@gmail.com>
2020-09-08 16:27:30 +0800

    add user route

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -9,6 +11,9 @@ func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/user/:name", func(c echo.Context) error {
+		return c.String(http.StatusOK,fmt.Sprintf("Hello, %s!",c.Param("name")))
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
